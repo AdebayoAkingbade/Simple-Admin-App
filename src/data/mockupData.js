9e9ad8dd23d5449b9fd0bad56f92233a87787231
Akingbacrown <aakingbade@ecobank.com>
2023-10-02 13:48:50 +0100

    initial commit

---
+++ src/data/mockupData.js
@@ -0,0 +1,20 @@
+export const data = [
+    {
+        id: 1,
+        name: "Adebayo Akingbade",
+        age: 31,
+        email: "adejeremih@gmail.com",
+        phone: "09018380825",
+        role: "admin"
+    },
+
+    {
+        id: 2,
+        name: "Wale Akingbade",
+        age: 38,
+        email: "akingbade@gmail.com",
+        phone: "07067387992",
+        role: "manager",
+        
+    }
+]
